129d5acfb0f19c6a0727c89665968b1a91def10b
Alyk Lippincott <lippinay@mail.uc.edu>
2024-09-24 17:16:35 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        double springCost = 200;
+        double summerCost = 300;
+        double fallCost = 250;
+        double winterCost = 400;
+        double totalYearlyCost = springCost + summerCost + fallCost + winterCost;
+        System.out.println("Total yearly maintenance cost: $" + totalYearlyCost);
+
+    }
+}
